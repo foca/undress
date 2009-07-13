ed0fcbc104b08badc2ce50cef89aaee0590019a5
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2009-07-13 02:02:00 -0300

    Oops, duplicated file

--- test/test_textile.rb
+++ test/test_textile.rb
@@ -7,7 +7,7 @@ class RainbowCloth::TextileTest < Test::Unit::TestCase
 
   context "Converting HTML to textile" do
     test "converts nested tags" do
-      assert_renders_textile "\n\nh2. _this is *very* important_\n\n", "<h2><em>this is <strong>very</strong> important</em></h2>"
+      assert_renders_textile "h2. _this is *very* important_\n", "<h2><em>this is <strong>very</strong> important</em></h2>"
     end
 
     context "inline elements" do
@@ -44,6 +44,11 @@ class RainbowCloth::TextileTest < Test::Unit::TestCase
       test "converts <del> tags" do
         assert_renders_textile "-foo bar-", "<del>foo bar</del>"
       end
+
+      test "converts <acronym> tags" do
+        assert_renders_textile "EPA(Environmental Protection Agency)", "<acronym title='Environmental Protection Agency'>EPA</acronym>"
+        assert_renders_textile "EPA", "<acronym>EPA</acronym>"
+      end
     end
 
     context "links" do
@@ -68,12 +73,12 @@ class RainbowCloth::TextileTest < Test::Unit::TestCase
 
     context "text formatting" do
       test "converts paragraphs" do
-        assert_renders_textile "\n\nfoo bar\n\n", "<p>foo bar</p>"
+        assert_renders_textile "foo\n\nbar\n", "<p>foo</p><p>bar</p>"
       end
 
       test "converts <pre> tags which only contain a <code> child" do
-        assert_renders_textile "\n\npc. var foo = 1;\n\n", "<pre><code>var foo = 1;</code></pre>"
-        assert_renders_textile "\n\npc. var foo = 1;\n\n", "<pre>   <code>var foo = 1;</code>   </pre>"
+        assert_renders_textile "pc. var foo = 1;\n", "<pre><code>var foo = 1;</code></pre>"
+        assert_renders_textile "pc. var foo = 1;\n", "<pre>   <code>var foo = 1;</code>   </pre>"
       end
 
       test "leaves <pre> tags which contain mixed content as HTML" do
@@ -81,87 +86,110 @@ class RainbowCloth::TextileTest < Test::Unit::TestCase
       end
 
       test "converts <br> into a new line" do
-        assert_renders_textile "\n", "<br/>"
+        assert_renders_textile "Foo\nBar", "Foo<br/>Bar"
       end
 
       test "converts blockquotes" do
-        assert_renders_textile "\n\nbq. foo bar\n\n", "<blockquote><div>foo bar</div></blockquote>"
+        assert_renders_textile "bq. foo bar\n", "<blockquote><div>foo bar</div></blockquote>"
       end
     end
 
     context "headers" do
       test "converts <h1> tags" do
-        assert_renders_textile "\n\nh1. foo bar\n\n", "<h1>foo bar</h1>"
+        assert_renders_textile "h1. foo bar\n", "<h1>foo bar</h1>"
       end
 
       test "converts <h2> tags" do
-        assert_renders_textile "\n\nh2. foo bar\n\n", "<h2>foo bar</h2>"
+        assert_renders_textile "h2. foo bar\n", "<h2>foo bar</h2>"
       end
 
       test "converts <h3> tags" do
-        assert_renders_textile "\n\nh3. foo bar\n\n", "<h3>foo bar</h3>"
+        assert_renders_textile "h3. foo bar\n", "<h3>foo bar</h3>"
       end
 
       test "converts <h4> tags" do
-        assert_renders_textile "\n\nh4. foo bar\n\n", "<h4>foo bar</h4>"
+        assert_renders_textile "h4. foo bar\n", "<h4>foo bar</h4>"
       end
 
       test "converts <h5> tags" do
-        assert_renders_textile "\n\nh5. foo bar\n\n", "<h5>foo bar</h5>"
+        assert_renders_textile "h5. foo bar\n", "<h5>foo bar</h5>"
       end
 
       test "converts <h6> tags" do
-        assert_renders_textile "\n\nh6. foo bar\n\n", "<h6>foo bar</h6>"
+        assert_renders_textile "h6. foo bar\n", "<h6>foo bar</h6>"
       end
     end
 
     context "lists" do
       test "converts bullet lists" do
-        assert_renders_textile "\n\n* foo\n* bar\n\n", "<ul><li>foo</li><li>bar</li></ul>"
+        assert_renders_textile "* foo\n* bar\n", "<ul><li>foo</li><li>bar</li></ul>"
       end
 
       test "converts numbered lists" do
-        assert_renders_textile "\n\n# foo\n# bar\n\n", "<ol><li>foo</li><li>bar</li></ol>"
+        assert_renders_textile "# foo\n# bar\n", "<ol><li>foo</li><li>bar</li></ol>"
       end
 
       test "converts nested bullet lists" do
-        assert_renders_textile "\n\n* foo\n** bar\n* baz\n\n", "<ul><li>foo<ul><li>bar</li></ul></li><li>baz</li></ul>"
+        assert_renders_textile "* foo\n** bar\n* baz\n", "<ul><li>foo<ul><li>bar</li></ul></li><li>baz</li></ul>"
       end
 
       test "converts nested numbered lists" do
-        assert_renders_textile "\n\n# foo\n## bar\n# baz\n\n", "<ol><li>foo<ol><li>bar</li></ol></li><li>baz</li></ol>"
+        assert_renders_textile "# foo\n## bar\n# baz\n", "<ol><li>foo<ol><li>bar</li></ol></li><li>baz</li></ol>"
       end
 
       test "converts nested mixed lists" do
-        assert_renders_textile "\n\n* foo\n## bar\n## baz\n*** quux\n* cuack\n\n",
+        assert_renders_textile "* foo\n## bar\n## baz\n*** quux\n* cuack\n",
                                "<ul><li>foo<ol><li>bar</li><li>baz<ul><li>quux</li></ul></li></ol></li><li>cuack</li></ul>"
       end
 
       test "converts a definition list" do
-        assert_renders_textile "\n\n- foo := defining foo =:\n- bar := defining bar =:\n\n",
+        assert_renders_textile "- foo := defining foo =:\n- bar := defining bar =:\n",
                                "<dl><dt>foo</dt><dd>defining foo</dd><dt>bar</dt><dd>defining bar</dd></dl>"
       end
     end
 
     context "tables" do
       test "converts a simple table" do
-        assert_renders_textile "\n\n|foo|bar|baz|\n|1|2|3|\n\n",
+        assert_renders_textile "|foo|bar|baz|\n|1|2|3|\n",
                                "<table><tr><td>foo</td><td>bar</td><td>baz</td></tr><tr><td>1</td><td>2</td><td>3</td></tr></table>"
       end
 
       test "converts a table with headers" do
-        assert_renders_textile "\n\n|_. foo|_. bar|_. baz|\n|1|2|3|\n\n",
+        assert_renders_textile "|_. foo|_. bar|_. baz|\n|1|2|3|\n",
                                "<table><tr><th>foo</th><th>bar</th><th>baz</th></tr><tr><td>1</td><td>2</td><td>3</td></tr></table>"
       end
 
       test "converts a table with cells that span multiple columns" do
-        assert_renders_textile "\n\n|foo|bar|baz|\n|\\2. 1|2|\n\n",
+        assert_renders_textile "|foo|bar|baz|\n|\\2. 1|2|\n",
                                "<table><tr><td>foo</td><td>bar</td><td>baz</td></tr><tr><td colspan='2'>1</td><td>2</td></tr></table>"
       end
 
       test "converts a table with cells that span multiple rows" do
-        assert_renders_textile "\n\n|/2. foo|bar|baz|\n|1|2|\n\n",
+        assert_renders_textile "|/2. foo|bar|baz|\n|1|2|\n",
                                "<table><tr><td rowspan='2'>foo</td><td>bar</td><td>baz</td></tr><tr><td>1</td><td>2</td></tr></table>"
+      end
+    end
+
+    context "applying post processing rules" do
+      test "compresses newlines to a maximum of two consecutive newlines" do
+        assert_renders_textile "Foo\n\nBar\n\nBaz\n\n* Quux 1\n* Quux 2\n", "<p>Foo</p><p>Bar</p><p>Baz</p><ul><li>Quux 1</li><li>Quux 2</li></p>"
+      end
+
+      test "strips trailing newlines from the start and end of the output string" do
+        assert_renders_textile "Foo\n", "<p>Foo</p>"
+      end
+
+      test "converts all fancy characters introduced by textile back into their 'source code'" do
+        assert_renders_textile "What the ... hell?", "What the &#8230; hell?"
+        assert_renders_textile "It's mine", "It&#8217;s mine"
+        assert_renders_textile "\"Fancy quoting\"", "&#8220;Fancy quoting&#8221;"
+        assert_renders_textile "How dashing--right?", "How dashing&#8212;right?"
+        assert_renders_textile "How dashing - right?", "How dashing &#8211; right?"
+        assert_renders_textile "2 x 2 = 4", "2 &#215; 2 = 4"
+        assert_renders_textile "2x2 = 4", "2&#215;2 = 4"
+        assert_renders_textile "Registered(r)", "Registered&#174;"
+        assert_renders_textile "Copyrighted(c)", "Copyrighted&#169;"
+        assert_renders_textile "Trademarked(tm)", "Trademarked&#8482;"
       end
     end
   end
