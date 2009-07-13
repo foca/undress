2aa3bd53baae6988b0d035202615a57bcfea0d91
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2009-07-13 00:00:18 -0300

    Clean the API a bit

--- lib/rainbow.rb
+++ lib/rainbow.rb
@@ -38,6 +38,14 @@ module Rainbow
           end.join("")
         end
 
+        def content_of(node)
+          if node.text?
+            node.to_s
+          else
+            process(node.children)
+          end
+        end
+
         def surrounded_by_whitespace?(node)
           node.previous.text? && node.previous.to_s =~ /\s+$/ || node.next.text? && node.next.to_s =~ /^\s+/
         end
@@ -58,63 +66,63 @@ module Rainbow
       # inline elements
       rule(:a) {|e|
         title = e.has_attribute?("title") ? " (#{e["title"]})" : ""
-        "[#{process(e.children)}#{title}:#{e["href"]}]"
+        "[#{content_of(e)}#{title}:#{e["href"]}]"
       }
       rule(:img) {|e|
         alt = e.has_attribute?("alt") ? "(#{e["alt"]})" : ""
         "!#{e["src"]}#{alt}!"
       }
-      rule(:strong) {|e| "*#{process(e.children)}*" }
-      rule(:em)     {|e| "_#{process(e.children)}_" }
-      rule(:code)   {|e| "@#{process(e.children)}@" }
-      rule(:cite)   {|e| "??#{process(e.children)}??" }
-      rule(:sup)    {|e| surrounded_by_whitespace?(e) ? "^#{process(e.children)}^" : "[^#{process(e.children)}^]" }
-      rule(:sub)    {|e| surrounded_by_whitespace?(e) ? "~#{process(e.children)}~" : "[~#{process(e.children)}~]" }
-      rule(:ins)    {|e| "+#{process(e.children)}+" }
-      rule(:del)    {|e| "-#{process(e.children)}-" }
+      rule(:strong) {|e| "*#{content_of(e)}*" }
+      rule(:em)     {|e| "_#{content_of(e)}_" }
+      rule(:code)   {|e| "@#{content_of(e)}@" }
+      rule(:cite)   {|e| "??#{content_of(e)}??" }
+      rule(:sup)    {|e| surrounded_by_whitespace?(e) ? "^#{content_of(e)}^" : "[^#{content_of(e)}^]" }
+      rule(:sub)    {|e| surrounded_by_whitespace?(e) ? "~#{content_of(e)}~" : "[~#{content_of(e)}~]" }
+      rule(:ins)    {|e| "+#{content_of(e)}+" }
+      rule(:del)    {|e| "-#{content_of(e)}-" }
 
       # text formatting and layout
-      rule(:p)          {|e| "\n\n#{process(e.children)}\n\n" }
+      rule(:p)          {|e| "\n\n#{content_of(e)}\n\n" }
       rule(:br)         {|e| "\n" }
-      rule(:blockquote) {|e| "bq. #{process(e.children)}\n" }
+      rule(:blockquote) {|e| "bq. #{content_of(e)}\n" }
       rule(:pre)        {|e|
         if e.children.all? {|n| n.text? && n.content =~ /^\s+$/ || n.elem? && n.name == "code" }
-          "pc. #{process((e % "code").children)}\n"
+          "pc. #{content_of(e % "code")}\n"
         else
-          "<pre>#{process(e.children)}</pre>"
+          "<pre>#{content_of(e)}</pre>"
         end
       }
 
       # headings
-      rule(:h1) {|e| "\n\nh1. #{process(e.children)}\n\n" }
-      rule(:h2) {|e| "\n\nh2. #{process(e.children)}\n\n" }
-      rule(:h3) {|e| "\n\nh3. #{process(e.children)}\n\n" }
-      rule(:h4) {|e| "\n\nh4. #{process(e.children)}\n\n" }
-      rule(:h5) {|e| "\n\nh5. #{process(e.children)}\n\n" }
-      rule(:h6) {|e| "\n\nh6. #{process(e.children)}\n\n" }
+      rule(:h1) {|e| "\n\nh1. #{content_of(e)}\n\n" }
+      rule(:h2) {|e| "\n\nh2. #{content_of(e)}\n\n" }
+      rule(:h3) {|e| "\n\nh3. #{content_of(e)}\n\n" }
+      rule(:h4) {|e| "\n\nh4. #{content_of(e)}\n\n" }
+      rule(:h5) {|e| "\n\nh5. #{content_of(e)}\n\n" }
+      rule(:h6) {|e| "\n\nh6. #{content_of(e)}\n\n" }
 
       # lists
       rule(:li) {|e|
         token = e.parent.name == "ul" ? "*" : "#"
         nesting = e.ancestors.inject(1) {|total,node| total + (%(ul ol).include?(node.name) ? 0 : 1) }
-        "\n#{token * nesting} #{process(e.children)}"
+        "\n#{token * nesting} #{content_of(e)}"
       }
       rule(:ul, :ol) {|e|
         if e.ancestors.detect {|node| %(ul ol).include?(node.name) }
-          process(e.children)
+          content_of(e)
         else
-          "\n#{process(e.children)}\n\n"
+          "\n#{content_of(e)}\n\n"
         end
       }
 
       # definition lists
-      rule(:dl) {|e| "\n\n#{process(e.children)}\n" }
-      rule(:dt) {|e| "- #{process(e.children)} " }
-      rule(:dd) {|e| ":= #{process(e.children)} =:\n" }
+      rule(:dl) {|e| "\n\n#{content_of(e)}\n" }
+      rule(:dt) {|e| "- #{content_of(e)} " }
+      rule(:dd) {|e| ":= #{content_of(e)} =:\n" }
 
       # tables
-      rule(:table) {|e| "\n\n#{process(e.children)}\n" }
-      rule(:tr) {|e| "#{process(e.children)}|\n" }
+      rule(:table) {|e| "\n\n#{content_of(e)}\n" }
+      rule(:tr) {|e| "#{content_of(e)}|\n" }
       rule(:td, :th) {|e|
         prefix = if e.name == "th"
           "_. "
@@ -124,7 +132,7 @@ module Rainbow
           "/#{e["rowspan"]}. "
         end
 
-        "|#{prefix}#{process(e.children)}" 
+        "|#{prefix}#{content_of(e)}" 
       }
     end
 
